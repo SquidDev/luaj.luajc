186d1fe31c3ce9e4460e10e9eac2a55da7ee1deb
SquidDev <bonzoweb@hotmail.co.uk>
2016-01-22 09:45:27 +0000

    Add an indent logger, for nicer debug prints
    Also update the branch upvalue code with some description
    and a time counter - I've spend far too long on this bug.

--- src/test/resources/org/squiddev/luaj/luajc/BranchUpvalue2.lua
+++ src/test/resources/org/squiddev/luaj/luajc/BranchUpvalue2.lua
@@ -1,17 +1,29 @@
-local upvalue
+--[[
+	Time spent on this bug: 4 hours
+
+	The issue lies in UpvalueInfo. The code believes that the second for loop writes to upvalue
+	as its phi values do not have an upvalue, and so it must be a write boundry.
+
+	0.11 (second for prep) and 0.15 (function call) do not have an upvalue and are phis of {0.0, 0.5} but 0.18
+	(second for loop) does.
+ ]]
+
+local upvalue = 1
 
 local function closure()
 	return upvalue
 end
 
 while true do
-	if "const" ~= "const" then
-		upvalue = "const"
+	if 1 == 2 then
+		upvalue = 1
 	else
-		for loop_num = "const", "const" do
-			for loop_iter in func() do
-				func(loop_num + loop_iter)
+		for loop_numA = 1, 2 do
+			for loop_numB = 1, 2 do
+				closure(loop_numA + loop_numB)
 			end
 		end
+
+		break
 	end
 end
